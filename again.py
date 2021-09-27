3d608f9ed27d4ed0eaf2ae63c534ca74473877a8
jurijsml <90174881+jurijsml@users.noreply.github.com>
2021-09-27 06:53:53 +0000

    Initial commit

---
+++ again.py
@@ -0,0 +1,5 @@
+'''
+Jurijs Malahovskis
+27.09.21
+'''
+lol = [tava saraksta elementi]
---
+++ main.py
@@ -0,0 +1,13 @@
+'''
+Jurijs Malahovskis
+27.09.21
+'''
+
+t=('lol', 'kek', 'ceburek')
+print(t)
+print(t[2])
+t[1] = 'lol'
+t[2] = 'kek'
+t[3] = 'ceburek'
+t.index('lol')
+t.index('ceburek')
